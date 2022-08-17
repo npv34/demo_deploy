4ff4ebd02095dd26bab5b2570bbbb70b8fb4b71e
npv34 <phanvanluan611996@gmail.com>
2022-08-17 09:56:43 +0700

    ..

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res, next) => {
-    res.end('<h1>Xin chao</h1>')
-})
 
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res, next) => {
+    res.end('<h1>Xin chao ban</h1>')
+})
 
 app.listen(port)
---
+++ public/js/my.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $.ajax({
+        url: 'https://localhost:3000/'
+    })
+})
